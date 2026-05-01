dc0e296bda3e79309bb9e153be794a61c52d0226
victorvzx <gifpa45we@gmail.com>
2026-05-01 16:45:05 -0300

    feat(vectors): optimization onthe maior_menor_float file

--- algoritmos/vetores/lista1/maior_menor_float.c
+++ algoritmos/vetores/lista1/maior_menor_float.c
@@ -5,6 +5,8 @@ int main(void){
 
     float maior = nums[0];
     float menor = nums[0];
+
+    int pos1, pos2;
     for(int i = 0; i < 8; i++){
         printf("Digite o %dº numero do vetor: ", i + 1);
         scanf("%f", &nums[i]);
@@ -14,14 +16,16 @@ int main(void){
             menor = nums[i];
         }
 
-        if(nums [i] > maior){
+        if(nums[i] > maior){
             maior = nums[i];
-        }else{
+            pos1 = i + 1;
+        }else if(nums[i] == 0 || nums[i] < menor){
             menor = nums[i];
+            pos2 = i + 1;
         }
     }
 
-    printf("Maior: %.2f\nMenor: %.2f\n", maior, menor);
+    printf("Maior: %.2f (Posição: %d)\nMenor: %.2f (Posição: %d)\n", maior, pos1, menor, pos2);
 
     return 0;
 }
