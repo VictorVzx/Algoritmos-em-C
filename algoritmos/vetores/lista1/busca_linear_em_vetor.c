7c9404c08e6ef8ae2faddbf59ff909f05aed676c
victorvzx <gifpa45we@gmail.com>
2026-05-01 17:54:11 -0300

    feat(vectors): add linear search algorythm

---
+++ algoritmos/vetores/lista1/busca_linear_em_vetor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+    int nums[10];
+
+    for(int i = 0; i < 10; i++){
+        printf("Digite o %dº numero: ", i + 1);
+        scanf("%d", &nums[i]);
+    }
+
+    int value;
+
+    printf("Digite o valor que você quer buscar: ");
+    scanf("%d", &value);
+
+    int achou = 0;
+
+    for(int i = 0; i < 10; i++){
+        if(value == nums[i]){
+            achou = 1;
+        }else{
+            achou = 0;
+        }
+
+        if(achou == 1){
+            printf("%d achado em: %d\n", value, i);
+        }
+    }
+
+    return 0;
+}
