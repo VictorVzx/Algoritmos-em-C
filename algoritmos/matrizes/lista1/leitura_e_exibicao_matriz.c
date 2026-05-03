1c1aee974bb78a041d2ff3d665de1cad45ead81e
victorvzx <gifpa45we@gmail.com>
2026-05-03 10:08:20 -0300

    feat(matrizes): changes in matrizes directory

--- algoritmos/matrizes/lista1/leitura_e_exibicao_matriz.c
+++ algoritmos/matrizes/lista1/leitura_e_exibicao_matriz.c
@@ -5,8 +5,14 @@ int main(void){
 
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            printf("Digite o %dº numero: ");
-            scanf
+            printf("Digite o %dº numero: ", j+1);
+            scanf("%d", &nums[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d", nums[i][j]);
         }
     }
     
